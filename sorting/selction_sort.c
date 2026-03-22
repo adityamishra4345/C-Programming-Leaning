8d3ec417ad530a20af1598b343b1a40ca5bcb2dd
adityamishra4345 <adityamishra4345@gmail.com>
2026-03-23 01:02:45 +0530

    Implimented Selection Sort

--- sorting/selction_sort.c
+++ sorting/selction_sort.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
 void selection_sort(int arr[],int n){
-    
+      for(int i=0;i<n;i++){
+        int min_idx=i;
+        for(int j=i;j<n-1;j++){
+            if (arr[j]<arr[min_idx]){
+                min_idx=j;
+            }
+        }
+        int temp=arr[min_idx];
+        arr[min_idx]=arr[i];
+        arr[i]=temp;
+}
+}
+int main(){
+    int arr[]={23,3,34,56,777,86,4,33,222};
+    int n=sizeof(arr)/4;
+    selection_sort(arr,n);
+    for(int i=0;i<n;i++){
+        printf("%d   ",arr[i]);
+    }
 }
